90e5bb087167eb0274022e7685e299d592f5eb3f
claudius <claudius.solomon@gmail.com>
2020-01-26 16:00:23 -0800

    express app.js post method

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var bodyParser = require('body-parser');
+var async  = require('express-async-await')
+var fetch = require('node-fetch')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,9 +23,28 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('dist'));
 
+app.get('/', (request, res) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+});
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+app.post('/tracks',(req,res) => {
+  fetch('https://api.deezer.com/chart/0/tracks')
+  .then(res => {
+    return res.text();
+  })
+  .then(body => {
+    let results = JSON.parse(body);
+    res.send(results);
+  });
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI',testAPIRouter);
