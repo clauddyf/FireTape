540b7e18033526dcc34d25ad6aae25010088511d
claudius <claudius.solomon@gmail.com>
2020-02-16 15:43:05 -0800

    mouseover work

--- dist/bubbles.js
+++ dist/bubbles.js
@@ -1,13 +1,5 @@
-
-
-// var fetch = require('node-fetch')
-// debugger
-
-
 const url ='http://localhost:9000/apiCall';
-// fetch(url)
-// .then(res => res.json())
-// .then(data => 
+
 d3.json(url)
 .then(function (tracks) {
         // debugger
@@ -44,14 +36,29 @@ d3.json(url)
                 name: d.title,
                 artist: d.artist.name,
                 rank: (d.position),
-                radius: radiusScale(d.rank / 400),
+                radius: radiusScale(d.rank /700 ),
                 image: d.album.cover_medium,
                 x: Math.random() * width,
                 y: heightScale((d.rank / 400))/*  Math.random() * height */
             }
         })
 
+        const hoverText = 
+        d3.select("#root svg")
+        .append("div")
+        // .attr("class", "hover")
+        // .text("hoverText")
+        .style("position", "absolute")
+        .style("visibility", "hidden")
+        .style('opacity',1)
+        .style("background-color", "pink")
+        .style("border-radius", "5px")
+        .style("z-index", "10")
+        .style("padding", "10px")
+        .style("color", "white")
         debugger
+
+        
         var defs = svg.append('defs');
     
         defs.selectAll('.poster-art')
@@ -69,31 +76,88 @@ d3.json(url)
             .attr("preserveAspectRatio", "none")
             .attr('xlink:href', d => d.image)
     
-        // debugger
+        debugger
         bubbles = d3.select('#root svg')
             .selectAll('circle')
             .data(nodes)
             .enter()
             .append('circle')
             .attr('r', d => { return d.radius })
+            .attr('text', d => d.name)
             .attr('fill', d => `url(#${d.rank})`)
-            .attr('stroke', d => { return d3.rgb('blue').darker() })
+            .on('mouseover',function(d){
+                hoverText
+                .html(
+                    `Track: ${d.name}<br/>
+                    Artist: $${d.artist}M<br/>
+                    Hotness Rank: ${d.rank} (based on weekend)<br/>`
+                    );
+                    debugger
+                    return hoverText.style('visibility','visible');
+            })
+            .on('mouseout',function(){
+                return hoverText.style('visibility','hidden');
+            })
             .call(d3.drag()
-                .on('start', dragStarted)
-                .on('drag', dragged)
-                .on('end', dragEnded)
+            .on('start', dragStarted)
+            .on('drag', dragged)
+            .on('end', dragEnded)
             )
-    
-        // debugger
-        let forceSimulation;
-    
-        forceSimulation = d3.forceSimulation()
+            
+            let forceSimulation;
+            
+            forceSimulation = d3.forceSimulation()
             .nodes(nodes)
             .velocityDecay(velocityDecay)
             .on('tick', ticked)
             .force('x', d3.forceX().strength(forceStrength).x(width / 2))
             .force('y', d3.forceY().strength(forceStrength).y(height / 2))
             .force("charge", d3.forceManyBody().strength(charge))
+            // .on('onmouseover', d => {
+            //     return hoverText
+            //       .transition()
+            //       .duration(200)
+            //       return hoverText
+            //       .style('opacity', 0.9)
+            //       .html((
+            //         `Track: ${d.name}<br/>
+            //          Artist: $${d.artist}M<br/>
+            //          Hotness Rank: ${d.rank} (based on weekend)<br/>`
+            //       ))
+            //       .style('left', d3.event.pageX + 'px')
+            //       .style('top', d3.event.pageY - 28 + 'px');
+            //   })
+            //   .on('onmouseout', () => {
+            //    return hoverText
+            //       .transition()
+            //       .duration(500)
+            //       .style('opacity', 0);
+            //   })
+            
+            
+            
+            
+            // .on("onmouseover", function (d,i){ //mouseover hoverText
+                // hoverText.html(
+                //   `Track: ${d.name}<br/>
+                //    Artist: $${d.artist}M<br/>
+                //    Hotness Rank: ${d.rank} (based on weekend)<br/>`
+                // );
+            //     hoverText.style("visibility", "visible");
+            //   })
+            // .on("onmousemove", () => {
+            //     return hoverText
+            //       .style("top", (d3.event.pageY+10)+"px")
+            //       .style("left",(d3.event.pageX+10)+"px");
+            //   })
+            // .on("onmouseout", () => hoverText.style("visibility", "hidden"))
+            // .call(d3.drag()
+            //     .on('start', dragStarted)
+            //     .on('drag', dragged)
+            //     .on('end', dragEnded)
+            // )
+            
+            // debugger
     
     
         function dragStarted(d) {
@@ -143,420 +207,3 @@ d3.json(url)
             return data;
         }
 })
-
-
-
-
-// d3.json(deezTracks).then(function (tracks) {
-//     debugger
-//     const height = 650;
-//     const width = 1300;
-//     var svg = d3.select('#root')
-//         .append('svg')
-//         .attr('height', height)
-//         .attr('width', width)
-//     const velocityDecay = 0.15;
-//     const forceStrength = 0.03;
-
-//     let nodes;
-//     let bubbles;
-//     let radiusScale;
-//     let colorScale;
-//     let heightScale;
-
-//     radiusScale = d3.scaleLinear()
-//         .domain([0, 500])
-//         .range([5, 30]);
-
-//     colorScale = d3.scaleSequential()
-//         .domain([0, 100])
-//         .interpolator(d3.interpolateRainbow);
-
-//     heightScale = d3.scaleLinear()
-//         .domain([0, 100])
-//         .range([0, height]);
-//     arrTracks = tracks.tracks.track
-//     nodes = arrTracks.map(d => {
-//         debugger
-//         return {
-//             name: d.name,
-//             artist: d.artist.name,
-//             rank: parseInt(d["@attr"].rank),
-//             radius: radiusScale(d.listeners / 400),
-//             image: Object.values(d.image[0])[0],
-//             x: Math.random() * width,
-//             y: heightScale((d.listeners / 400))/*  Math.random() * height */
-//         }
-//     })
-
-//     // debugger
-//     // nodes = nasa.photos.map(d => {
-//     //     // debugger
-//     //     return {
-//     //         name: d.camera.name,
-//     //         roverName: d.rover.name,
-//     //         rank: d.id,
-//     //         radius: d.id/2000,
-//     //         // rank: Object.values(d).id,
-//     //         image: d.img_src
-//     //     }
-//     // })
-
-
-//     debugger
-//     var defs = svg.append('defs');
-
-//     defs.selectAll('.poster-art')
-//         .data(nodes)
-//         .enter()
-//         .append('pattern')
-//         .attr('class', 'poster-art')
-//         .attr('id', d => d.rank)
-//         .attr('height', '100%')
-//         .attr('width', '100%')
-//         .attr("patternContentUnits", "objectBoundingBox")
-//         .append('image')
-//         .attr('height', 1.5)
-//         .attr('width', 1)
-//         .attr("preserveAspectRatio", "none")
-//         .attr('xlink:href', d => d.image)
-
-//     // debugger
-//     bubbles = d3.select('#root svg')
-//         .selectAll('circle')
-//         .data(nodes)
-//         .enter()
-//         .append('circle')
-//         .attr('r', d => { return d.radius })
-//         .attr('fill', d => `url(#${d.rank})`)
-//         .attr('stroke', d => { return d3.rgb('blue').darker() })
-//         .call(d3.drag()
-//             .on('start', dragStarted)
-//             .on('drag', dragged)
-//             .on('end', dragEnded)
-//         )
-
-//     // debugger
-//     let forceSimulation;
-
-//     forceSimulation = d3.forceSimulation()
-//         .nodes(nodes)
-//         .velocityDecay(velocityDecay)
-//         .on('tick', ticked)
-//         .force('x', d3.forceX().strength(forceStrength).x(width / 2))
-//         .force('y', d3.forceY().strength(forceStrength).y(height / 2))
-//         .force("charge", d3.forceManyBody().strength(charge))
-
-
-//     function dragStarted(d) {
-//         console.log('start');
-//         forceSimulation.alphaTarget(0.3).restart()
-//     }
-//     function dragged(d) {
-//         console.log('drag');
-//         /* bubbles.attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y); */
-//         d.fx = d3.event.x
-//         d.fy = d3.event.y
-//     }
-
-//     function dragEnded(d) {
-//         console.log('end');
-//         delete d.fx;
-//         delete d.fy;
-//         forceSimulation.alphaTarget(0);
-//     }
-//     // debugger
-//     function ticked() {
-//         bubbles
-//             .attr("cx", function (d) {
-//                 return d.x;
-//             })
-//             .attr("cy", function (d) {
-//                 return d.y;
-//             });
-//     }
-
-//     function radius(d) {
-//         return d.radius + 1
-//     }
-
-//     function charge(d) {
-//         return -Math.pow(d.radius, 2) * forceStrength;
-//     }
-
-
-//     function generateRandomData() {
-//         const data = [];
-//         for (let i = 0; i < 200; i++) {
-//             data.push(
-//                 { randomNumber: Math.round(Math.random() * 100) }
-//             )
-//         }
-//         return data;
-//     }
-
-// })
-
-
-// // const height = 600;
-// // const width = 1000;
-// const velocityDecay = 0.15;
-// const forceStrength = 0.03;
-
-// let nodes;
-// let bubbles;
-
-// let rawData = generateRandomData();
-// let forceSimulation;
-
-// let radiusScale;
-// let colorScale;
-// let heightScale;
-
-
-// radiusScale = d3.scaleLinear()
-//     .domain([0, 100])
-//     .range([5, 30]);
-
-// colorScale = d3.scaleSequential()
-//     .domain([0, 100])
-//     .interpolator(d3.interpolateRainbow);
-
-// heightScale = d3.scaleLinear()
-//     .domain([0, 100])
-//     .range([0, height]);
-
-// nodes = rawData.map(d => {
-//     debugger
-//     return {
-//         radius: radiusScale(d.randomNumber),
-//         fill: colorScale(d.randomNumber),
-//         x: Math.random() * width,
-//         y: heightScale(d.randomNumber)/*  Math.random() * height */
-//     }
-// })
-
-// /* console.log('node ', nodes);
-// console.log('data', rawData); */
-
-// /* nodes.sort((a, b) => b.radius - a.radius) */
-
-
-
-
-// d3.select('#root')
-//     .append('svg')
-//     .attr('height', height)
-//     .attr('width', width)
-
-// debugger
-// bubbles = d3.select('#root svg')
-//     .selectAll('circle')
-//     .data(nodes)
-//     .enter()
-//     .append('circle')
-//     .attr('r', d => { return d.radius })
-//     .attr('fill', d => 'salmon')
-//     .attr('stroke', d => { return d3.rgb('salmon').darker() })
-//     .call(d3.drag()
-//         .on('start', dragStarted)
-//         .on('drag', dragged)
-//         .on('end', dragEnded)
-//     )
-
-
-
-// forceSimulation = d3.forceSimulation()
-//     .nodes(nodes)
-//     .velocityDecay(velocityDecay)
-//     .on('tick', ticked)
-//     .force('x', d3.forceX().strength(forceStrength).x(width / 2))
-//     .force('y', d3.forceY().strength(forceStrength).y(height / 2))
-//     .force("charge", d3.forceManyBody().strength(charge))
-
-
-// function dragStarted(d) {
-//     console.log('start');
-//     forceSimulation.alphaTarget(0.3).restart()
-// }
-// function dragged(d) {
-//     console.log('drag');
-//     /* bubbles.attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y); */
-//     d.fx = d3.event.x
-//     d.fy = d3.event.y
-// }
-
-// function dragEnded(d) {
-//     console.log('end');
-//     delete d.fx;
-//     delete d.fy;
-//     forceSimulation.alphaTarget(0);
-// }
-
-// function ticked() {
-//     bubbles
-//         .attr("cx", function (d) {
-//             return d.x;
-//         })
-//         .attr("cy", function (d) {
-//             return d.y;
-//         });
-// }
-
-// function radius(d) {
-//     return d.radius + 1
-// }
-
-// function charge(d) {
-//     return -Math.pow(d.radius, 2) * forceStrength;
-// }
-
-
-// function generateRandomData() {
-//     const data = [];
-//     for (let i = 0; i < 200; i++) {
-//         data.push(
-//             { randomNumber: Math.round(Math.random() * 100) }
-//         )
-//     }
-//     return data;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//THEEEEEEE FRICKINNNNNNNNNN WINNNNNERRRRRRRRRR
-
-// var diameter = 900;
-// var width = 960,
-// height = 500
-// // var color = d3.scaleOrdinal(d3.schemeCategory20);
-// var color = d3.scale.category20();
-// // debugger
-// var bubble = d3.layout.pack()
-// .size([diameter,diameter])
-// .padding(2.2);
-
-// var svg = d3.select('body')
-// .append('svg')
-// .attr('width',diameter)
-// .attr('height', diameter)
-// .attr('class','bubble');
-
-// d3.json('http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=jamaica&api_key=a0d411fa7c676669092342d66c4913d8&format=json&limit=15', function(error, tracks) {
-//     // var data = music.tracks
-//     // debugger
-//     var counts = [];
-//     tracks.tracks.track.forEach(function(d) {
-//         // debugger
-//         arr = d.listeners
-//         counts.push(arr)
-//     })
-//     console.log(counts)
-
-//     var data = [];
-//     for(var key in counts) {
-//         var val = counts[key];
-//         data.push({
-//             count: val,
-//             track: key
-//         });
-//     }
-//     console.log(data)
-
-//     data = data.map(function(d){
-//         d.value = +d['count'];
-//         return d;
-//     });
-
-//     var nodes = bubble.nodes({
-//         children: data
-//     }).filter(function(d) {
-//         return !d.children;
-//     })
-
-//     var bubbles = svg.append('g')
-//         .attr("transform", "translate(0,0)")
-//         .selectAll('.bubble')
-//         .data(nodes)
-//         .enter();
-
-//     bubbles.append('circle')
-//         .attr('r',function(d){
-//             return d.r;
-//         })
-//         .attr('cx',function(d){
-//             return d.x;
-//         })
-//         .attr('cy',function(d){
-//             return d.y;
-//         })
-//         .transition()
-//             .attr('transform','translate(25,50)')
-//             .ease('elastic')
-//             .duration(3000)
-//             .style("fill", function(d) { return color(d.value); });
-
-//     bubbles.append('text')
-//         .attr('x', function(d) {
-//             return d.x
-//         })
-//         .attr('y',function(d) {
-//             return d.y + 5;
-//         })
-//         .attr('text-anchor', 'middle')
-//         .text(function(d){
-//                 // debugger
-//             return d['value'];
-//         })
-//         .style({
-//             'fill':'white',
-//             "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
-//             "font-size": "9px"
-//         })
-//         function charge(d) {
-//             return -Math.pow(d.radius, 2.0) / 8;
-//           }
-//           var force = d3.layout
-//             .force()
-//             .size([width, height])
-//             .charge(charge) // <- Using the charge function in the force layout
-//             .gravity(-0.01)
-//             .friction(0.9)
-//         force.on('tick',function(){
-//             nodes.attr("cx", function(d) { return d.x; })
-//             .attr("cy", function(d) { return d.y; });
-//         })
-
-
-
-//     // debugger
-// });
-
